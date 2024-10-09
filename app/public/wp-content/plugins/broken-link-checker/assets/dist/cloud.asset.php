780088d5ed577066f362b0456a8bf9c2ac4f7a94
Isabelle.Ancelin <77393135+isabelle-ancelin@users.noreply.github.com>
2024-10-09 17:49:10 +0200

    first commit

---
+++ app/public/wp-content/plugins/broken-link-checker/assets/dist/cloud.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element', 'wp-i18n'), 'version' => '880efbed43e35a0163f3');
---
+++ app/public/wp-content/plugins/broken-link-checker/assets/dist/local-topnav.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-element'), 'version' => '25192d61995926b127fb');
